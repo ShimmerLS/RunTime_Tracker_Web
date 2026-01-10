bbd10a0f559148bcd96a52531b22036e1f6f35b7
ShimmerLS <66188400+ShimmerLS@users.noreply.github.com>
2026-01-10 09:59:19 +0800

    Change SITE_TITLE to '欸嘿~你好~' in config.js

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ const config = {
     dev: {
         API_BASE: import.meta.env.VITE_API_BASE || 'https://api-usage.1812z.top/api', // 默认开发环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.1812z.top/',
-        SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
+        SITE_TITLE: import.meta.env.VITE_SITE_TITLE || '欸嘿~你好~'
     },
     prod: {
         API_BASE: import.meta.env.VITE_API_BASE || 'https://api-usage.1812z.top/api', // 默认生产环境
         ADMIN_URL: import.meta.env.VITE_ADMIN_URL || 'https://runtime-tracker-admin.1812z.top/',
-        SITE_TITLE: import.meta.env.VITE_SITE_TITLE || 'RunTime Tracker'
+        SITE_TITLE: import.meta.env.VITE_SITE_TITLE || '欸嘿~你好~'
     }
 };
 
